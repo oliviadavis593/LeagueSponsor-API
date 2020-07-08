bde42e75794105e5c08fca2671de1d1b2d204f6b
Olivia Davis <olivia.davis593@gmail.com>
2020-07-08 13:22:33 -0500

    ssc & router error

--- src/leagues/leagues-router.js
+++ src/leagues/leagues-router.js
@@ -18,23 +18,43 @@ const serializeLeague = league => ({
 leaguesRouter
     .route('/')
     .get((req, res, next) => {
-        const knexInstance = req.app.get('db');
-        LeaguesService.getAllLeagues(knexInstance)
-            .then(leagues => {
-                res.json(leagues.map(serializeLeague));
-            })
-            .catch(next)
-    })
-    .get((req, res, next) => {
-        if (!req.query.latitude || !req.query.longitude || !req.query.budget) {
-            res.status(400).json({ error: 'Insufficent data provided' });
-        }
-        else {
-            LeaguesService.findLeagues(req.query).then((leagues) => {
-                res.status(200).json(leagues);
-            })
-            .catch(next)
-        }
+       const knexInstance = req.app.get('db')
+       const { latitude, longitude, budget, radius } = req.params; 
+
+       if (!latitude || !longitude || !budget) {
+           return res.status(400).json({ error: 'Insuffecient parameters provided!' })
+       } else {
+           /*
+            This is a random assumption of what 5 miles radius might be
+           */
+          const ifWithinFiveMiles = (league) => {
+              if (radius) {
+                  return league.latitude - latitude <= radius && league.longitude - longitude <= radius;
+              } else {
+                return (
+                    league.latitude - latitude >= 2 &&
+                    league.latitude - latitude <= 4 &&
+                    league.longitude - longitude >= 2 &&
+                    league.longitude - longitude <= 4
+                );
+              }
+          };
+          const withinRadius = leagues.filter(ifWithinFiveMiles).sort((a, b) =>
+            a.price > b.price ? 1 : -1,
+          );
+          let sum = 0; 
+          const withinBudget = [];
+          withinRadius.forEach((league) => {
+              if (sum + league.price > budget || league.price > budget) {
+                  return;
+              }
+              else {
+                  withinBudget.push(league);
+                  sum += league.price; 
+              }
+          });
+          return res.status(200).json(withinBudget)
+       }
     })
     .post(jsonParser, (req, res, next) => {
         const { league_name, website, location, latitude, longitude, price } = req.body;
